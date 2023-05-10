48b71a2ac0862e731b662d6083c16994008caab8
emielpa <115221852+emielpa@users.noreply.github.com>
2023-05-10 16:19:15 +0200

    PaybackPeriodAskingPricesAndFrequency.py

--- Thesis Code/Ventilation Investment analysis code/PaybackPeriodAskingPricesAndFrequency.py
+++ Thesis Code/Ventilation Investment analysis code/PaybackPeriodAskingPricesAndFrequency.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 frequentie = [50,40,30,20]
-savings36 = [0,1592, 2345,3235]
-savings45 = [0, 1949, 2871,3960]
-savings27 = [0,1235, 1820,2511]
+
+savings36 = np.array([0,1387.42, 2120.62,2931.78])
+savings45 = (savings36/0.36)*0.45
+savings27 = (savings36/0.36)*0.27
 
 costOneDrivePerFan = 6757.686
 costMultipleFansPerDrive = 5074.914 
 costDriveInstalled = 292
-optiFluxSubs = [200,300,400,500,600,700,800,900,1000]
+optiFluxSubs = [200,300,400,500,600,700]
 
 pbOneDrive3640 = []
 pbMultipleFan3640 = []
@@ -90,16 +91,16 @@ plt.figure(1)
 plt.title("Payback period with respect to subscription price at 40Hz")
 plt.xlabel("Yearly subscription per cold store [€]")
 plt.ylabel("Payback period [years]")
-plt.plot(optiFluxSubs,pbOneDrive2740, label = "1 fan/1 drive 0,27 €/kWh", color = "blue", linestyle = "solid")
-plt.plot(optiFluxSubs,pbOneDrive3640, label = "1 fan/1 drive 0,36 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive2740, label = "1 fan/1 drive 0,27 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive3640, label = "1 fan/1 drive 0,36 €/kWh", color = "blue", linestyle = "solid")
 plt.plot(optiFluxSubs,pbOneDrive4540, label = "1 fan/1 drive 0,45 €/kWh", color = "blue", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbMultipleFan2740, label = "2 fans/1 drive 0,27 €/kWh", color = "orange", linestyle = "solid")
-plt.plot(optiFluxSubs,pbMultipleFan3640, label = "2 fans/1 drive 0,36 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan2740, label = "2 fans/1 drive 0,27 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan3640, label = "2 fans/1 drive 0,36 €/kWh", color = "orange", linestyle = "solid")
 plt.plot(optiFluxSubs,pbMultipleFan4540, label = "2 fans/1 drive 0,45 €/kWh", color = "orange", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbInstalled2740, label = "Drive installed 0,27 €/kWh", color = "red", linestyle = "solid")
-plt.plot(optiFluxSubs,pbInstalled3640, label = "Drive installed 0,36 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled2740, label = "Drive installed 0,27 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled3640, label = "Drive installed 0,36 €/kWh", color = "red", linestyle = "solid")
 plt.plot(optiFluxSubs,pbInstalled4540, label = "Drive installed 0,45 €/kWh", color = "red", linestyle = "dashed")
 plt.grid()
 plt.legend()
@@ -108,16 +109,16 @@ plt.figure(2)
 plt.title("Payback period with respect to subscription price 30Hz")
 plt.xlabel("Yearly subscription per cold store [€]")
 plt.ylabel("Payback period [years]")
-plt.plot(optiFluxSubs,pbOneDrive2730, label = "One drive 0,27 €/kWh", color = "blue", linestyle = "solid")
-plt.plot(optiFluxSubs,pbOneDrive3630, label = "One drive 0,36 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive2730, label = "One drive 0,27 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive3630, label = "One drive 0,36 €/kWh", color = "blue", linestyle = "solid")
 plt.plot(optiFluxSubs,pbOneDrive4530, label = "One drive 0,45 €/kWh", color = "blue", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbMultipleFan2730, label = "Multiple fans per drive 0,27 €/kWh", color = "orange", linestyle = "solid")
-plt.plot(optiFluxSubs,pbMultipleFan3630, label = "Multiple fans 0,36 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan2730, label = "Multiple fans per drive 0,27 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan3630, label = "Multiple fans 0,36 €/kWh", color = "orange", linestyle = "solid")
 plt.plot(optiFluxSubs,pbMultipleFan4530, label = "Multiple fans 0,45 €/kWh", color = "orange", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbInstalled2730, label = "Drive Installed 0,27 €/kWh", color = "red", linestyle = "solid")
-plt.plot(optiFluxSubs,pbInstalled3630, label = "Drive Installed 0,36 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled2730, label = "Drive Installed 0,27 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled3630, label = "Drive Installed 0,36 €/kWh", color = "red", linestyle = "solid")
 plt.plot(optiFluxSubs,pbInstalled4530, label = "Drive Installed 0,45 €/kWh", color = "red", linestyle = "dashed")
 plt.grid()
 plt.legend()
@@ -126,20 +127,18 @@ plt.figure(3)
 plt.title("Payback period with respect to subscription price 20Hz")
 plt.xlabel("Yearly subscription per cold store [€]")
 plt.ylabel("Payback period [years]")
-plt.plot(optiFluxSubs,pbOneDrive2720, label = "One drive 0,27 €/kWh", color = "blue", linestyle = "solid")
-plt.plot(optiFluxSubs,pbOneDrive3620, label = "One drive 0,36 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive2720, label = "One drive 0,27 €/kWh", color = "blue", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbOneDrive3620, label = "One drive 0,36 €/kWh", color = "blue", linestyle = "solid")
 plt.plot(optiFluxSubs,pbOneDrive4520, label = "One drive 0,45 €/kWh", color = "blue", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbMultipleFan2720, label = "Multiple fans per drive 0,27 €/kWh", color = "orange", linestyle = "solid")
-plt.plot(optiFluxSubs,pbMultipleFan3620, label = "Multiple fans 0,36 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan2720, label = "Multiple fans per drive 0,27 €/kWh", color = "orange", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbMultipleFan3620, label = "Multiple fans 0,36 €/kWh", color = "orange", linestyle = "solid")
 plt.plot(optiFluxSubs,pbMultipleFan4520, label = "Multiple fans 0,45 €/kWh", color = "orange", linestyle = "dashed")
 
-plt.plot(optiFluxSubs,pbInstalled2720, label = "Drive Installed 0,27 €/kWh", color = "red", linestyle = "solid")
-plt.plot(optiFluxSubs,pbInstalled3620, label = "Drive Installed 0,36 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled2720, label = "Drive Installed 0,27 €/kWh", color = "red", linestyle = "dotted")
+plt.plot(optiFluxSubs,pbInstalled3620, label = "Drive Installed 0,36 €/kWh", color = "red", linestyle = "solid")
 plt.plot(optiFluxSubs,pbInstalled4520, label = "Drive Installed 0,45 €/kWh", color = "red", linestyle = "dashed")
 plt.grid()
 plt.legend()
 
 plt.show()
-
-
